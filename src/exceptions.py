06db581278353945200818c0937fa6026f2816af
Gilberto F S Junior <gilberto.junior@dclick.com.br>
2026-03-20 09:17:56 -0300

    commit 2 - atualização

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,5 +1,26 @@
+import dataclasses
+
+
 class ErroNegocio(Exception):
     pass
 
+
 class ErroTecnico(Exception):
     pass
+
+
+@dataclasses.dataclass
+class CamposNBS:
+    """Campos obrigatórios para lançamento no NBS.
+    Tipado e nomeado para evitar desalinhamento posicional.
+    """
+    cpf: str
+    cod_empresa: str
+    cod_filial: str
+    processo: str
+    iniciar_em: str
+    despesa_pagamento: str
+    protocolo: str
+    centro_custo: str
+    data_vencimento: str
+    tipo_pagamento: str
